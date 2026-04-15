f02c197c7481fdbcc4f711344198aa95f1beb2f2
aloluvsu <preciadoalondra758@gmail.com>
2026-04-15 13:45:53 -0700

    Initial Commit

---
+++ Wu-tang App/Wu_tang_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wu_tang_AppApp.swift
+//  Wu-tang App
+//
+//  Created by PRECIADO, ALONDRA on 4/15/26.
+//
+
+import SwiftUI
+
+@main
+struct Wu_tang_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Wu-tang AppTests/Wu_tang_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Wu_tang_AppTests.swift
+//  Wu-tang AppTests
+//
+//  Created by PRECIADO, ALONDRA on 4/15/26.
+//
+
+import Testing
+@testable import Wu_tang_App
+
+struct Wu_tang_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
